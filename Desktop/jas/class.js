5b4e16de6c323c098e3c380d09dbbe7f012f3287
mac <mac@macs-MacBook-Pro.local>
2023-05-15 13:09:43 +0100

    class

---
+++ Desktop/jas/class.js
@@ -0,0 +1,51 @@
+// const names = "Sixtus Grace Imo Uty Emem Frank Janet Tommy Joy Gift Peace Regina Donald Precious Noble"
+// let student = names.split(' ');
+// console.log(student);
+
+// let classList = [
+
+//     { Name: 'Sixtus',  class: "A"},
+  
+//     { Name: 'Grace',  class: "A"},
+  
+//     { Name: 'Imo',  class: "A"},
+
+//     { Name: 'Tommy',  class: "A"},
+
+//     { Name: 'Joy',  class: "A"},
+
+//     { Name: 'Utibe',  class: "B"},
+
+//     { Name: 'Emem',  class: "B"},
+
+//     { Name: 'Frank',  class: "B"},
+
+//     { Name: 'Janet',  class: "B"},
+
+//     { Name: 'Gift',  class: "B"},
+
+//     { Name: 'Peace',  class: "C"},
+
+//     { Name: 'Regina',  class: "C"},
+
+//     { Name: 'Donald',  class: "C"},
+
+//     { Name: 'Precious',  class: "C"},
+
+//     { Name: 'Noble',  class: "C"}
+  
+//   ];
+//   console.log(classList)
+
+//   for (let i = 0; i < classList.length; i ++ ){
+
+//     console.log(classList[i]);
+    
+    // }
+
+
+
+
+
+
+    
